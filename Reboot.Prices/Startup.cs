e0d9d6816cc255d378e27b92b230fddb0bd6b022
Konstantine <Konstaphy@gmail.com>
2024-05-21 23:09:42 +0500

    feat: добавил докеры для микросервиса цен

---
+++ Reboot.Prices/Startup.cs
@@ -0,0 +1,31 @@
+﻿namespace Reboot.Prices;
+
+public class Startup(IConfiguration configuration)
+{
+    public IConfiguration Configuration { get; } = configuration;
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("*", builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+        });
+
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseCors("*");
+
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+    }
+}
