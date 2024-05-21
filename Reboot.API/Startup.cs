e500e6407cf29fd8b3bf7872d07995ff5aa7feff
Konstantine <Konstaphy@gmail.com>
2024-05-21 23:02:54 +0500

    feat: добавил оцелот

---
+++ Reboot.API/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Reboot.API;
+
+public class Startup(IConfiguration configuration)
+{
+    public IConfiguration Configuration { get; } = configuration;
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("*", builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+        });
+        services.AddMvc();
+        services.AddOcelot(Configuration);
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+        app.UseCors("*");
+        app.UseOcelot().Wait();
+    }
+}
